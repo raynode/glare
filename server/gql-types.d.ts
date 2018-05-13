6c391df283cbc28ea2ae04c4e2fa8333d1f5375c
Tobias Kopelke <tobias.kopelke@liquidlabs.de>
2018-05-13 20:04:16 +0200

    Whitespace & Formatting

--- server/gql-types.d.ts
+++ server/gql-types.d.ts
@@ -2,63 +2,62 @@
 // graphql typescript definitions
 
 declare namespace GQL {
-interface IGraphQLResponseRoot {
-data?: IQuery | IMutation;
-errors?: Array<IGraphQLResponseError>;
-}
+  interface IGraphQLResponseRoot {
+    data?: IQuery | IMutation;
+    errors?: Array<IGraphQLResponseError>;
+  }
 
-interface IGraphQLResponseError {
-/** Required for all errors */
-message: string;
-locations?: Array<IGraphQLResponseErrorLocation>;
-/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-[propName: string]: any;
-}
+  interface IGraphQLResponseError {
+    /** Required for all errors */
+    message: string;
+    locations?: Array<IGraphQLResponseErrorLocation>;
+    /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+    [propName: string]: any;
+  }
 
-interface IGraphQLResponseErrorLocation {
-line: number;
-column: number;
-}
+  interface IGraphQLResponseErrorLocation {
+    line: number;
+    column: number;
+  }
 
-interface IQuery {
-__typename: "Query";
-uptime: number | null;
-}
+  interface IQuery {
+    __typename: "Query";
+    uptime: number | null;
+  }
 
-interface IMutation {
-__typename: "Mutation";
-}
+  interface IMutation {
+    __typename: "Mutation";
+  }
 
-interface IAUTHPROVIDERAUTH0 {
-idToken: string;
-}
+  interface IAUTHPROVIDERAUTH0 {
+    idToken: string;
+  }
 
-interface IUser {
-__typename: "User";
-ID: string;
-auth0UserId: string;
-created: any;
-updated: any;
-given_name: string | null;
-family_name: string | null;
-nickname: string | null;
-name: string | null;
-picture: string | null;
-gender: string | null;
-locale: string | null;
-email: string | null;
-email_verified: boolean | null;
-}
+  interface IUser {
+    __typename: "User";
+    ID: string;
+    auth0UserId: string;
+    created: any;
+    updated: any;
+    given_name: string | null;
+    family_name: string | null;
+    nickname: string | null;
+    name: string | null;
+    picture: string | null;
+    gender: string | null;
+    locale: string | null;
+    email: string | null;
+    email_verified: boolean | null;
+  }
 
-type Node = IUser;
+  type Node = IUser;
 
-interface INode {
-__typename: "Node";
-ID: string;
-created: any | null;
-updated: any | null;
-}
+  interface INode {
+    __typename: "Node";
+    ID: string;
+    created: any | null;
+    updated: any | null;
+  }
 }
 
 // tslint:enable
-
