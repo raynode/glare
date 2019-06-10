0572c6f54f37a8f6aee68955cd67a0b9026b2c2c
Tobias K <nox@raynode.de>
2019-06-10 21:50:23 +0200

    remove debug

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,6 +4,4 @@ import * as Knex from 'knex'
 const env = process.env.NODE_ENV || 'development'
 if (!config.hasOwnProperty(env)) throw new Error(`Could not find database configuration for env: ${env}`)
 
-console.log(config[env])
-
 export const db = Knex(config[env])
