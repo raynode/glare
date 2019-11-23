6f3c080ba9cb53bbd2f8c6188d958da007dc4ecb
Tobias K <nox@raynode.de>
2019-11-23 21:01:06 +0100

    logs

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,13 @@
 import { config } from 'db/config'
 import * as Knex from 'knex'
+import { create } from 'services/logger'
+
+const log = create('db')
 
 const env = process.env.NODE_ENV || 'development'
+log(`selected environment: ${env}`)
 if (!config.hasOwnProperty(env)) throw new Error(`Could not find database configuration for env: ${env}`)
+
+log.info(config[env])
 
 export const db = Knex(config[env])
